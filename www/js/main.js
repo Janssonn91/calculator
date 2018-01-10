3c6f97ecdd1a610f9a36cf753a15dff7cd435c23
Unknown <lecke.martin@gmail.com>
2018-01-10 13:10:20 +0100

    CurrentValue and MathArray Eventhandler

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,36 @@
 class Calculator {
 	constructor(){
 		this.display = "0";
+    this.mathArray = [];
 		this.render();
+    this.eventHandler();
 	}
 
 	render() {
 		$('.viewer').empty().append(this.display);
 	}
+
+
+  eventHandler() {
+    let that = this;
+    let currentValue = "";
+    $(document).on('click', '.num', function() {
+      currentValue += $(this).data('num');
+    });
+    $(document).on('click', '.ops', function() {
+      that.mathArray.push(currentValue);
+      if ($(this).data('ops') == "times") {
+        that.mathArray.push('*');
+      } else if ($(this).data('ops') == "divided") {
+        that.mathArray.push('/');
+      } else if ($(this).data('ops') == "minus") {
+        that.mathArray.push('-');
+      } else if ($(this).data('ops') == "plus") {
+        that.mathArray.push('+');
+      }
+      currentValue = "";
+    });
+  }
 
 }
 
