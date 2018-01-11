385025d2876022643ff61496fd9378fed9bcd504
Unknown <lecke.martin@gmail.com>
2018-01-11 17:49:55 +0100

    Indentation fix

--- www/js/main.js
+++ www/js/main.js
@@ -1,57 +1,57 @@
 class Calculator {
 	constructor(){
 		this.display = "0";
-    this.mathArray = [];
+		this.mathArray = [];
 		this.render();
-    this.eventHandler();
+		this.eventHandler();
 	}
 
 	render() {
 		$('.viewer').empty().append(this.display);
 	}
 
-  calculate() {
-    let allValues = "";
-    for (let valAndOps of this.mathArray) {
-      allValues += valAndOps;
-    }
-    let result = eval(allValues);
-    this.display = result;
-    this.render();
-  }
+	calculate() {
+		let allValues = "";
+		for (let valAndOps of this.mathArray) {
+			allValues += valAndOps;
+		}
+		let result = eval(allValues);
+		this.display = result;
+		this.render();
+	}
 
-  eventHandler() {
-    let that = this;
-    let currentValue = "";
-    let displayValue = "";
+	eventHandler() {
+		let that = this;
+		let currentValue = "";
+		let displayValue = "";
 		let operator;
 		let last;
-    $(document).on('click', '.num', function() {
-      currentValue += $(this).data('num');
+		$(document).on('click', '.num', function() {
+			currentValue += $(this).data('num');
 			that.display = currentValue;
-      displayValue = currentValue;
+			displayValue = currentValue;
 			that.render();
-    });
+		});
 
-    $('#equals').on('click', function() {
-      if (currentValue) {
-        that.mathArray.push(currentValue);
-        that.calculate()
-      }
-      currentValue = "";
-    });
-
-    $(document).on('click', '.ops', function() {
+		$('#equals').on('click', function() {
 			if (currentValue) {
-      	that.mathArray.push(currentValue);
+				that.mathArray.push(currentValue);
+				that.calculate()
+			}
+			currentValue = "";
+		});
+
+		$(document).on('click', '.ops', function() {
+			if (currentValue) {
+				that.mathArray.push(currentValue);
 			}
 
 			operator = $(this).data('ops');
 			last = that.mathArray[that.mathArray.length-1];
 			console.log(last);
 			if ( '+-/*'.includes(last) ){
-        that.display.slice(-1);
-        that.display += operator;
+				that.display.slice(-1);
+				that.display += operator;
 				that.mathArray[that.mathArray.length-1] = operator;
 			} else {
 				that.mathArray.push(operator);
@@ -61,8 +61,8 @@ class Calculator {
 			currentValue = "";
 			that.render();
 
-    });
-  }
+		});
+	}
 
 }
 
